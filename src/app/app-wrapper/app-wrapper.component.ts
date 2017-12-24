d3b3abc71c63c77c5ce1c4fdf9d57da1e7978cca
Shumail Mohyuddin <shumailmohyuddin@gmail.com>
2017-12-24 16:43:10 +0100

    refactor: render image dynamically

--- src/app/app-wrapper/app-wrapper.component.ts
+++ src/app/app-wrapper/app-wrapper.component.ts
@@ -1,11 +1,14 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import * as Cropper from 'cropperjs';
 
 /*  - reset on again choosing
     - preview while cropping
     - show preview in placeholder
     - generate canvas of 2
-    - downlaod 
+    - downlaod
+    - get coordinate of placeholder from base image
+    - canvas drag & drop
+    - canvas draw dynamically from image height
 */
 
 @Component({
@@ -17,7 +20,7 @@ export class AppWrapperComponent implements OnInit {
 
   cropper: Cropper;
   isImageLoaded = false;
-  constructor(private elementRef: ElementRef) { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
 
@@ -25,10 +28,16 @@ export class AppWrapperComponent implements OnInit {
 
   readFile($event): void {
     const reader = new FileReader();
-    const imageToPreview = this.elementRef.nativeElement.querySelector('.imageToPreview');
+    const imagePreviewDiv = this.elementRef.nativeElement.querySelector('.image_preview');
+    const img = this.renderer.createElement('img');
+    this.renderer.setAttribute(img, 'id', 'image');
+    this.renderer.addClass(img, 'imageToPreview');
+
     reader.onload = (e: any) => {
       this.isImageLoaded = true;
-      imageToPreview.src = e.target.result;
+      this.renderer.setAttribute(img, 'src', e.target.result);
+      this.renderer.appendChild(imagePreviewDiv, img);
+      // imageToPreview.src = e.target.result;
       this.enableCropping();
     };
     console.log($event.target.files[0]);
