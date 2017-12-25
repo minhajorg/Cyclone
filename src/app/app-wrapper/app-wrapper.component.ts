4a2bec7c8d3c4d3013ef5ca2013e5725410db75d
Shumail Mohyuddin <shumailmohyuddin@gmail.com>
2017-12-25 02:35:02 +0100

    fix: download on IE & jpg select in Safari

--- src/app/app-wrapper/app-wrapper.component.ts
+++ src/app/app-wrapper/app-wrapper.component.ts
@@ -114,9 +114,41 @@ export class AppWrapperComponent implements OnInit, AfterViewInit {
   downloadCanvas(link) {
     console.log(link);
     const filename = 'cyclone_' + new Date().getTime() + '.png';
-    const canvasRef = <HTMLCanvasElement> document.getElementById('canvas');
-    link.href = canvasRef.toDataURL();
-    link.download = filename;
+    const canvasRef = <HTMLCanvasElement>document.getElementById('canvas');
+    const canvasb64URL = canvasRef.toDataURL().replace(/^data:[a-z]*;,/, '');
+
+    if (navigator.appVersion.toString().indexOf('.NET') > 0) { // for IE browser
+      const contentType = 'image/png';
+      window.navigator.msSaveBlob(this.b64toBlob(canvasb64URL, contentType), filename);
+    } else {
+      link.href = canvasb64URL;
+      link.download = filename;
+    }
+  }
+  /* hack for IE - https://stackoverflow.com/questions/16245767/creating-a-blob-from-a-base64-string-in-javascript
+   */
+  b64toBlob(b64Data, contentType, sliceSize?) {
+    contentType = contentType || '';
+    sliceSize = sliceSize || 512;
+    b64Data = b64Data.split(',')[1];
+    const byteCharacters = atob(b64Data);
+    const byteArrays = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+      const byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      const byteArray = new Uint8Array(byteNumbers);
+
+      byteArrays.push(byteArray);
+    }
+
+    const blob = new Blob(byteArrays, { type: contentType });
+    return blob;
   }
 
 }
